09b5065baf501c0852cdcf6f044d7e0ce04c5727
Zudkin Pavel <zudkinpa@mpei.ru>
2022-10-16 13:24:18 +0300

    6. @RequestParam

--- src/main/java/ru/zudkin/springcourse/controllers/FirstController.java
+++ src/main/java/ru/zudkin/springcourse/controllers/FirstController.java
@@ -14,9 +14,8 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request) {
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
 
         System.out.println("Hello, " + name + " " + surname);
 
