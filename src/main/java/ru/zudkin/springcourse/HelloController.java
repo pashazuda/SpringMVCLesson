b01dc9a1ea254262ab179aeb4ed5444518be56b8
Zudkin Pavel <zudkinpa@mpei.ru>
2022-10-16 12:11:23 +0300

    Java config

--- src/main/java/ru/zudkin/springcourse/HelloController.java
+++ src/main/java/ru/zudkin/springcourse/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello-world")
+    @GetMapping("/helloworld")
     public String sayHello() {
         return "hello_world";
     }
