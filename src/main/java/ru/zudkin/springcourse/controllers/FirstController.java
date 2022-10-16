e31e5dcba38108b9c9634424e391ba59239d01d1
Zudkin Pavel <zudkinpa@mpei.ru>
2022-10-16 13:15:43 +0300

    6. HttpServletRequest

--- src/main/java/ru/zudkin/springcourse/controllers/FirstController.java
+++ src/main/java/ru/zudkin/springcourse/controllers/FirstController.java
@@ -1,8 +1,12 @@
 package ru.zudkin.springcourse.controllers;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
@@ -10,7 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        System.out.println("Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
